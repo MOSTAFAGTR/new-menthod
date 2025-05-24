d319b9b6b1e063487990b0507bf85ec26d2bcb40
mostafa <mostafa2206274@miuegypt.edu.eg>
2025-05-24 11:28:55 +0300

    otta

--- x.cpp
+++ x.cpp
@@ -4,9 +4,8 @@ using namespace std;
 int main(){
 
     cout<<"hello man"<<endl;
-    cout<<"hello man"<<endl;
+    cout<<"hello world"<<endl;
       cout<<"hello man"<<endl;
-        cout<<"hello man"<<endl;
-          cout<<"hello man"<<endl;
+       
    
 }
